e4632bc93b7ffb0cf9307020d5f61bc1c427c9af
Elena Rudenko <mantikore@list.ru>
2019-11-25 18:07:38 +0100

    active items highlighted

--- src/app/components/wish-list/wish-list.component.ts
+++ src/app/components/wish-list/wish-list.component.ts
@@ -13,6 +13,7 @@ export class WishListComponent implements OnInit {
   products: Observable<Product[]>;
   price: Observable<number>;
   @Output() selected = new EventEmitter<Product>();
+  clicked;
 
   constructor(
     private productsService: ProductService
@@ -25,6 +26,7 @@ export class WishListComponent implements OnInit {
 
   onSelect(product: Product) {
     this.selected.emit(product);
+    this.clicked = product;
   }
 
   clearWishlist() {
